0a0e6a0d81ad81c5998e7b92053ce7f791af9e49
Weerawut <weerawut@live.com>
2025-01-21 09:49:35 +0700

    Initial Commit

---
+++ ViewBuilderPractice/ViewBuilderPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderPracticeApp.swift
+//  ViewBuilderPractice
+//
+//  Created by Weerawut Chaiyasomboon on 21/1/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
